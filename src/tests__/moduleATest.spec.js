8c5950528ea316c6e10536606da039899cafa7be
Ezra Jeonadab G. Del Rosario <ezrajeonadabg@gmail.com>
2025-10-01 00:03:59 +0800

    test: added test case for V to 5

--- src/tests__/moduleATest.spec.js
+++ src/tests__/moduleATest.spec.js
@@ -2,5 +2,10 @@ const roman_numeral_converter = require('../moduleA')
 
 // Checks correct conversion of Roman Numeral 'I' to 1.
 test('Converts I to 1', () => {
-    expect(roman_numeral_converter('I')).toBe(1)
+    expect(roman_numeral_converter('I')).toBe(1);
+});
+
+// Checks correct conversion of Roman Numeral 'V' to 5.
+test('Converts V to 5', () => {
+    expect(roman_numeral_converter('V')).toBe(5);
 });
